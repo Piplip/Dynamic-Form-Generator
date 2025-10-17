8159c3e09777790bea8160dcfe8c3db6bcffa8c7
Piplip <dnguyenkhanh457@gmail.com>
2025-10-17 18:48:10 +0700

    Revert "initial"
    This reverts commit 7f8ce01de3500ab6bc403e4a91b1eb7a635fc2ea.

--- src/utils/string.ts
+++
@@ -1,3 +0,0 @@
-export function toCamelCase(str: string): string {
-  return str.replace(/\s(.)/g, function($1) { return $1.toUpperCase(); }).replace(/\s/g, '').replace(/^(.)/, function($1) { return $1.toLowerCase(); });
-}
